2c8a5d44f95278c91b04491bce3aded8b0f12de2
M. Fazri Nizar <anon6372098.id@gmail.com>
2025-12-13 08:40:11 +0700

    build: adjust for android and ios platform

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import Flutter
 import UIKit
+import flutter_background_service_ios
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,9 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    // Register plugins for background service
+    SwiftFlutterBackgroundServicePlugin.taskIdentifier = "com.mydosen.background.refresh"
+    
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
