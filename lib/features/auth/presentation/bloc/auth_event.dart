98dc4769cfe80eebc86d3acff2bdde8a6711b67f
M. Fazri Nizar <anon6372098.id@gmail.com>
2025-12-13 08:36:08 +0700

    feat(auth): add auth bloc events

---
+++ lib/features/auth/presentation/bloc/auth_event.dart
@@ -0,0 +1,24 @@
+import 'package:equatable/equatable.dart';
+
+abstract class AuthEvent extends Equatable {
+  const AuthEvent();
+
+  @override
+  List<Object?> get props => [];
+}
+
+class CheckAuthStatusEvent extends AuthEvent {}
+
+class LoginEvent extends AuthEvent {
+  final String email;
+  final String password;
+
+  const LoginEvent({required this.email, required this.password});
+
+  @override
+  List<Object?> get props => [email, password];
+}
+
+class GetProfileEvent extends AuthEvent {}
+
+class LogoutEvent extends AuthEvent {}
